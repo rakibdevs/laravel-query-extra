0494ae8eeefb93621d8ef6e29114e1fe5a26d76a
Md. Rakibul Islam <rakib1708@gmail.com>
2021-03-24 16:33:54 +0600

    Add Invalid Query Exception

---
+++ src/Exceptions/InvalidQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RakibDevs\QueryExtra\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class InvalidQuery extends ExceptionHandler
+{
+    
+}
